e3d1d7bfe4c922193d36b44eb3a29357fa104695
Juan Marco Buffa <juanchocone@gmail.com>
2026-05-04 20:30:44 -0300

    clase 4

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "**",
+        redirectTo: "login"
+    }
+];
